73b1831a49be7888d143bd93466bb663c43c4bc8
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-09-27 08:13:53 -0700

    moved methods to bottom, with declares at top

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+void boardViz(char board[3][3]);
+bool checkWin(char board[3][3]);
+int choice(char move[2]);
+
+int main(){
+  char grid[3][3];
+  char moveChoice[2];
+  boardViz(grid);
+
+  cout << "Please type a move choice(i.e. a2, c3). X goes first." << endl;
+  cin.get(moveChoice, 2);
+  cin.get();
+}
+
 void boardViz(char board[3][3]){
   cout << " 1 2 3" << endl;
   cout << "a" << board[0][0] << " " << board [0][1] << " " << board[0][2] << endl;
@@ -17,15 +31,4 @@ bool checkWin(char board[3][3]){
 
 int choice(char move[2]){
 
-}  
-
-int main(){
-  char grid[3][3];
-  char moveChoice[2];
-  boardViz(grid);
-
-  cout << "Please type a move choice(i.e. a2, c3). X goes first." << endl;
-  cin.get(moveChoice, 2);
-  cin.get();
-}
-
+} 
