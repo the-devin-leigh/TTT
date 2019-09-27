fe17d6d62d92a75140c6b67284d66574a546d153
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-09-27 08:10:10 -0700

    boardViz method working, set up input

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 void boardViz(char board[3][3]){
   cout << " 1 2 3" << endl;
+  cout << "a" << board[0][0] << " " << board [0][1] << " " << board[0][2] << endl;
+  cout << "b" << board[1][0] << " " << board [1][1] << " " << board[1][2] << endl;
+  cout << "c" << board[2][0] << " " << board [2][1] << " " << board[2][2] << endl;
+  
 }
 
-int main(){
+bool checkWin(char board[3][3]){
 
+}
+
+int choice(char move[2]){
+
+}  
+
+int main(){
+  char grid[3][3];
+  char moveChoice[2];
+  boardViz(grid);
+
+  cout << "Please type a move choice(i.e. a2, c3). X goes first." << endl;
+  cin.get(moveChoice, 2);
+  cin.get();
 }
 
