ffe172f89b4ef3b46cc35400d95c3b0e1c89e64f
U-BSD\351693 <351693@SHS-10DEC3222.beaverton.k12.or.us>
2019-10-01 08:45:53 -0700

    choice method implemented but not quite working

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 void boardViz(char board[3][3]);
 bool checkWin(char board[3][3]);
-int choice(char move[2]);
+void choice(char move[2], char board[3][3], bool XO);
 
 int main(){
   char grid[3][3];
   char moveChoice[2];
-  boardViz(grid);
+  bool play = true;
+  bool playLetter = true; //true = X
 
-  cout << "Please type a move choice(i.e. a2, c3). X goes first." << endl;
-  cin.get(moveChoice, 2);
-  cin.get();
+  while(play == true){
+    boardViz(grid);
+
+    cout << "Please type a move choice(i.e. a2, c3)." << endl;
+    cin.get(moveChoice, 2);
+    cin.get();
+    choice(moveChoice, grid, playLetter);
+    boardViz(grid);
+  } 
 }
 
 void boardViz(char board[3][3]){
@@ -22,13 +29,40 @@ void boardViz(char board[3][3]){
   cout << "a" << board[0][0] << " " << board [0][1] << " " << board[0][2] << endl;
   cout << "b" << board[1][0] << " " << board [1][1] << " " << board[1][2] << endl;
   cout << "c" << board[2][0] << " " << board [2][1] << " " << board[2][2] << endl;
-  
 }
 
 bool checkWin(char board[3][3]){
 
 }
 
-int choice(char move[2]){
+void choice(char move[2], char board[3][3], bool XO){
+  /* if(move == "a1"){
+    
+  }else if(move == "a2"){
+    
+  }else if(move == "a3"){
+    
+  }else if(move == "b1"){
+    
+  }else if(move == "b2"){
+    
+  }else if(move == "b3"){
+    
+  }else if(move == "c1"){
+    
+  }else if(move == "c2"){
+    
+  }else if(move == "c3"){
+    
+  }*/
 
-} 
+  int row = move[0] - 'a';
+  int column = move[1] - '1';
+  if(XO == true){
+    board[row][column] = 'X';
+    XO = false;
+  }else{
+    board[row][column] = 'O';
+    XO = true;
+  }  
+}
