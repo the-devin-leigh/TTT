6fbb44fea26aadc492734ec127b176b848f11b3b
the-devin-leigh <dkhleigh@gmail.com>
2019-10-10 22:05:42 -0700

    everything looks fine, but not sure what goes wrong with a1 input?? honestly no clue

--- TTTNoArray.cpp
+++ TTTNoArray.cpp
@@ -8,128 +8,169 @@
 
 using namespace std;
 
-int status(char grid[9]);
-void printBoard(char grid[9]);
+int status(char grid[3][3]);
+void printBoard(char grid[3][3]);
 
 int main(){
-  char grid[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-  int player = 1;
-  int ifRun;
+  char grid[3][3] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+  char player = 'X';
   char choice[2];
   char letter;
   bool still = true;
   int winX = 0;
   int winO = 0;
   char stillChoice;
-  
+
+  //game loop
   while(still == true){
     do{
       printBoard(grid);
-      if(player%2 == 1){
-	letter = 'X';
-      }else{
-	letter = 'O';
-      }
-      
+
+      //introductory statement
       cout << "TicTacToe" << endl;
       cout << "(Player 1 is X, Player 2 is O)" << endl;
-      cout << "Player" << player << ", enter your move (ex. a2, c3): ";
+      if(player == 'X'){
+	cout << "Player 1 enter your move (ex. a2, c3): ";
+      }else if(player == 'O'){
+	cout << "Player 2 enter your move (ex. a2, c3) ";
+      }
       cin >> choice;
-      
-      if(tolower(choice[0]) == 'a' && choice[1] == '1'){
-	grid[0] = letter;
-      }else if(tolower(choice[0]) == 'a' && choice[1] == '2'){
-	grid[1] = letter;
-      }else if(tolower(choice[0]) == 'a' && choice[1] == '3'){
-	grid[2] = letter;
-      }else if(tolower(choice[0]) == 'b' && choice[1] == '1'){
-	grid[3] = letter;
-      }else if(tolower(choice[0]) == 'b' && choice[1] == '2'){
-	grid[4] = letter;
-      }else if(tolower(choice[0]) == 'b' && choice[1] == '3'){
-	grid[5] = letter;
-      }else if(tolower(choice[0]) == 'c' && choice[1] == '1'){
-	grid[6] = letter;
-      }else if(tolower(choice[0]) == 'c' && choice[1] == '2'){
-	grid[7] = letter;
-      }else if(tolower(choice[0]) == 'c' && choice[1] == '3'){
-	grid[8] = letter;
-      }else{
-	cout << "Invalid";
-	player--;
-	cin.ignore();
-	cin.get();
-      }
-      ifRun = status(grid);
-      player++;
-    }while(ifRun == -1);
-    
-    printBoard(grid);
-    
-    if(ifRun == 1){
-      cout << "Player " << player << " wins!";
-      if(player%2 == 1){
-	winX++;
-	cout << "Player " << player << " wins: " << winX << endl;
-	cout << "Player " << ++player << " wins: " << winO << endl;
-      }else{
-	winO++;
-	cout << "Player " << player << " wins: " << winX << endl;
-	cout << "Player " << --player << " wins: " << winO << endl;
-      }
-    }else{
-      cout << "It's a tie!";
-      if(player%2 == 1){
-	cout << "Player " << player << " wins: " << winX << endl;
-	cout << "Player " << ++player << " wins: " << winO << endl;
-      }else{
-	cout << "Player " << player << " wins: " << winX << endl;
-	cout << "Player " << --player << " wins: " << winO << endl;
-      }
-    }
-    cin.ignore();
-    cin.get();
 
+      //input to board
+      if(tolower(choice[0]) == 'a' && choice[1] == '1' && grid[0][0] == ' '){
+	grid[0][0] = player;
+      }else if(tolower(choice[0]) == 'a' && choice[1] == '2' && grid[0][1] == ' '){
+	grid[0][1] = player;
+      }else if(tolower(choice[0]) == 'a' && choice[1] == '3' && grid[0][2] == ' '){
+	grid[0][2] = player;
+      }else if(tolower(choice[0]) == 'b' && choice[1] == '1' && grid[1][0] == ' '){
+	grid[1][0] = player;
+      }else if(tolower(choice[0]) == 'b' && choice[1] == '2' && grid[1][1] == ' '){
+	grid[1][1] = player;
+      }else if(tolower(choice[0]) == 'b' && choice[1] == '3' && grid[1][2] == ' '){
+	grid[1][2] = player;
+      }else if(tolower(choice[0]) == 'c' && choice[1] == '1' && grid[2][0] == ' '){
+	grid[2][0] = player;
+      }else if(tolower(choice[0]) == 'c' && choice[1] == '2' && grid[2][1] == ' '){
+	grid[2][1] = player;
+      }else if(tolower(choice[0]) == 'c' && choice[1] == '3' && grid[2][2] == ' '){
+	grid[2][2] = player;
+      }else{
+	cout << "Invalid" << endl;
+      }
+
+      //toggles players
+      if(player == 'X'){
+	player = 'O';
+      }else{
+	player = 'X';
+      }
+      
+    }
+    //while game has not ended, continue doing code above
+    while(status(grid) == -1);
+    printBoard(grid);
+
+    //win statements
+    if(status(grid) == 1){
+      cout << "Player 1 wins!" << endl;
+      winX++;
+      cout << "Player 1 Wins: " << winX << endl;
+      cout << "Player 2 Wins: " << winO << endl;
+    }else if(status(grid) == 2){
+      cout << "Player 2 wins!" << endl;
+      winO++;
+      cout << "Player 1 Wins: " << winX << endl;
+      cout << "Player 2 Wins: " << winO << endl;
+    }else{
+      cout << "It's a tie!" << endl;
+      cout << "Player 1 Wins: " << winX << endl;
+      cout << "Player 2 Wins: " << winO << endl;
+    }
+
+    // checks for player to play again or not
     cout << "Keep playing? (y/n)" << endl;
     cin >> stillChoice;
     if(tolower(stillChoice) == 'y'){
       still = true;
+      for(int i = 0; i < 3; i++){
+	for(int j = 0; j < 3; j++){
+	  grid[i][j] = ' ';
+	}
+      }
     }else if(tolower(stillChoice) == 'n'){
       still = false;
     }else{
       cout << "Invalid entry" << endl;
-    }  
+    }
+   
   }  
 }
 
-int status(char grid[9]){
-  if(grid[0] == grid[1] && grid[1] == grid[2]){ //a row
+
+// checks for wins
+int status(char grid[3][3]){
+  if(grid[0][0] == 'X' && grid[0][1] == 'X' && grid[0][2] == 'X'){ //a row
     return 1;
-  }else if (grid[3] == grid[4] && grid[4] == grid[5]){ //b row
+  }
+  if (grid[1][0] == 'X' && grid[1][1] == 'X' && grid[1][2] == 'X'){ //b row
     return 1;
-  }else if (grid[6] == grid[7] && grid[7] == grid[8]){ //c row
+  }
+  if (grid[2][0] == 'X' && grid[2][1] == 'X' && grid[2][2] == 'X'){ //c row
     return 1;
-  }else if (grid[0] == grid[3] && grid[3] == grid[6]){ //1 column
+  }
+  if (grid[0][0] == 'X' && grid[1][0] == 'X' && grid[2][0] == 'X'){ //1 column
     return 1;
-  }else if (grid[1] == grid[4] && grid[4] == grid[7]){ //2 column
+  }
+  if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid[2][1] == 'X'){ //2 column
     return 1;
-  }else if (grid[2] == grid[5] && grid[5] == grid[8]){ //3 column
+  }
+  if (grid[0][2] == 'X' && grid[1][2] == 'X' && grid[2][2] == 'X'){ //3 column
     return 1;
-  }else if (grid[0] == grid[4] && grid[4] == grid[8]){ //top left to bottom right 
+  }
+  if (grid[0][0] == 'X' && grid [1][1] == 'X' && grid[2][2] == 'X'){ //top left to bottom right 
     return 1;
-  }else if (grid[2] == grid[4] && grid[4] == grid[6]){ //top right to bottom left
+  }
+  if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X'){ //top right to bottom left
     return 1;
-  }else if(grid[0] != ' ' && grid[1] != ' ' && grid[2] != ' ' && grid[3] != ' ' && grid[4] != ' ' && grid[5] != ' ' && grid[6] != ' ' && grid[7] != ' ' && grid[8] != ' '){ //tie
+  }
+
+  if(grid[0][0] == 'O' && grid[0][1] == 'O' && grid[0][2] == 'O'){ //a row
+    return 2;
+  }
+  if (grid[1][0] == 'O' && grid[1][1] == 'O' && grid[1][2] == 'O'){ //b row
+    return 2;
+  }
+  if (grid[2][0] == 'O' && grid[2][1] == 'O' && grid[2][2] == 'O'){ //c row
+    return 2;
+  }
+  if (grid[0][0] == 'O' && grid[1][0] == 'O' && grid[2][0] == 'O'){ //1 column
+    return 2;
+  }
+  if (grid[0][1] == 'O' && grid[1][1] == 'O' && grid[2][1] == 'O'){ //2 column
+    return 2;
+  }
+  if (grid[0][2] == 'O' && grid[1][2] == 'O' && grid[2][2] == 'O'){ //3 column
+    return 2;
+  }
+  if (grid[0][0] == 'O' && grid [1][1] == 'O' && grid[2][2] == 'O'){ //top left to bottom right 
+    return 2;
+  }
+  if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O'){ //top right to bottom left
+    return 2;
+  }
+  
+  if(grid[0][0] != ' ' && grid[0][1] != ' ' && grid[0][2] != ' ' && grid[1][0] != ' ' && grid[1][1] != ' ' && grid[1][2] != ' ' && grid[2][0] != ' ' && grid[2][1] != ' ' && grid[2][2] != ' '){ //tie
     return 0;
-  }else{
-    return -1;
-  } 
+  }
+
+  return -1;
 }
 
-void printBoard(char grid[9]){
+// prints board to console
+void printBoard(char grid[3][3]){
   cout << ' ' << '1' << '2' << '3' << endl;
-  cout << 'a' << grid[0] << grid[1] << grid[2] << endl;
-  cout << 'b' << grid[3] << grid[4] << grid[5] << endl;
-  cout << 'c' << grid[6] << grid[7] << grid[8] << endl;
+  cout << 'a' << grid[0][0] << grid[0][1] << grid[0][2] << endl;
+  cout << 'b' << grid[1][0] << grid[1][1] << grid[1][2] << endl;
+  cout << 'c' << grid[2][0] << grid[2][1] << grid[2][2] << endl;
 }
-
